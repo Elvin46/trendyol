0339f68ec5dc0ee448a6fa472e6919cd81fe60b2
Elvin46 <elvinva@code.edu.az>
2023-03-11 04:18:27 +0400

    initial

--- trendyol/index.js
+++ trendyol/index.js
@@ -1,4 +1,21 @@
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
-})
+// var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+// var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+//   return new bootstrap.Dropdown(dropdownToggleEl)
+// })
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+const btn = document.querySelector(".menu-btn")
+const collapse = document.querySelector(".nav-menu-collapse")
+btn.addEventListener('click' , function() {
+    collapse.classList.toggle("visible")
+});
