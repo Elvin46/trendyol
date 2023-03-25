ff100a1d709b628ae21adabf8fd73cb5d77b9016
Elvin46 <elvinva@code.edu.az>
2023-03-25 06:19:17 +0400

    HomePage Full Finish with Header

--- trendyol/js/index.js
+++ trendyol/js/index.js
@@ -20,3 +20,22 @@ const collapse = document.querySelector(".sidebar")
 btn.addEventListener('click' , function() {
     collapse.classList.toggle("visible")
 });
+
+
+const profileBtn = document.querySelector('.my-profile');
+let profileOpen = false;
+profileBtn.addEventListener('click', () => {
+  // if(!menuOpen) {
+  //   menuBtn.classList.add('open');
+  //   menuOpen = true;
+  // } else {
+  //   menuBtn.classList.remove('open');
+  //   menuOpen = false;
+  // }
+  profileBtn.classList.toggle('open');
+});
+const btn1 = document.querySelector(".my-profile")
+const collapse1 = document.querySelector(".sidebar_profile")
+btn1.addEventListener('click' , function() {
+    collapse1.classList.toggle("visible")
+});
